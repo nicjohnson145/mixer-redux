6a30155a4007c15464773d23ae284f4929f809e9
Nic Johnson <nicjohnson145@hotmail.com>
2019-11-22 22:34:29 -0600

    Better presentation of drink items

--- mixer/lib/presentation/drink_item.dart
+++ mixer/lib/presentation/drink_item.dart
@@ -8,20 +8,37 @@ class DrinkItem extends StatelessWidget {
         @required this.drink,
     });
 
+    Widget getCardContents(BuildContext context) {
+        return Container(
+            child: Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: 5.0,
+                    horizontal: 10.0,
+                ),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                        Text(
+                            this.drink.name,
+                            style: Theme.of(context).textTheme.title,
+                        ),
+                        Text(
+                            this.drink.primary_alcohol,
+                            style: Theme.of(context).textTheme.subhead,
+                        )
+                    ],
+                ),
+            ),
+        );
+    }
+
     @override
     Widget build(BuildContext context) {
-        return ListTile(
-            title: Text(
-                this.drink.name,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.title,
-            ),
-            subtitle: Text(
-                this.drink.primary_alcohol,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.subhead,
+        return Card(
+            child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: () => print('TAPPED'),
+                child: this.getCardContents(context),
             ),
         );
     }
